7af9749f1e2ce6ec2bb4213919f44f793d3ea7f0
Davide staffieri <davide@protoweb.me>
2022-01-14 01:37:42 +0000

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# References:
+# - https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-centos-7
+# - https://forum.sys-adm.in/t/swap-vds-centos/2445
+
+
+function _installSwap() {
+	read -p 'How Gb need to Swap file?: ' _swap
+
+	# dd if=/dev/zero of=/swapfile count=4096 bs=1MiB
+	fallocate -l ${_swap}G /swapfile
+	chmod 600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+	echo -e "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
+}
+
+if grep -q "/swapfile" /etc/fstab ; then
+    echo "/swapfile - already installed!"
+    exit 1
+else
+    _installSwap
+    echo "Done!"
+    exit 0
+fi
+
+# Optional
+# sudo sysctl vm.swappiness=10
+
+# echo -e "vm.swappiness = 10" >> /etc/sysctl.conf
+# echo -e "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
